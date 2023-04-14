19e02b358f755b5030a3a67b726103339b376d63
Lina Bajirao <61426043+Leena0050@users.noreply.github.com>
2023-04-14 16:54:33 +0530

    Update Try.java

--- Try.java
+++ Try.java
@@ -3,4 +3,5 @@ public class main
 public static void main(String[]args){
     System.out.println("Trying Github");
     System.out.println("again tryin in github");
+    System.out.println("Did this on Github");
 }
